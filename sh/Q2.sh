ba503e4a520371bedb3ea3df0f23e9f3af5b96d8
Ivanpang <ivan.pang@pinnacle-holdings.hk>
2021-02-14 00:50:14 +0800

    [ANSWER] Q2

--- sh/Q2.sh
+++ sh/Q2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
 NAME_TAG=$1
-TAG_VALUE=$2
-ssh ec2-user@$(aws ec2 describe-instances --filters "Name=tag:${NAME_TAG},Values=${TAG_VALUE}" --query "Reservations[*].Instances[*].PublicIpAddress" --output=text)
+
+ssh ec2-user@$(aws ec2 describe-instances --filters "Name=tag:${NAME_TAG},Values=" --query "Reservations[*].Instances[*].PublicIpAddress" --output=text)
