f6e04e19e473303c1212bb1f70820ccd932ca96d
Ivanpang <ivan.pang@pinnacle-holdings.hk>
2021-02-14 00:55:03 +0800

    [ANSWER] Q2

--- sh/Q2.sh
+++ sh/Q2.sh
@@ -2,4 +2,11 @@
 
 NAME_TAG=$1
 
-ssh ec2-user@$(aws ec2 describe-instances --filters "Name=tag:${NAME_TAG},Values=" --query "Reservations[*].Instances[*].PublicIpAddress" --output=text)
+ip=$(aws ec2 describe-instances --filters "Name=tag:${NAME_TAG},Values=" --query "Reservations[*].Instances[*].PublicIpAddress" --output=text)
+if [ -z "${ip}" ]
+then
+  echo "Host not found"
+else
+  echo ssh ec2-user@"${ip}"
+fi
+
